079aff2aa6cebcc600cc1a03fce0d2024de86125
Victor Lellis <32583593+vWernay@users.noreply.github.com>
2022-12-23 12:07:58 -0300

    🔧 Update fxmanifest.lua
    Some refactor + change the version checker github.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
+fx_version 'bodacious'
 game 'rdr3'
-fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 lua54 'yes'
@@ -23,4 +23,4 @@ ui_page 'ui/hud.html'
 version '1.0'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
-vorp_github 'https://github.com/VORPCORE/VORP-Metabolism'
+vorp_github 'https://github.com/DX-BR/vorp_metabolism-lua'
